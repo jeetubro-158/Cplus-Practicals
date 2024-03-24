477f25059e0688235d55beee6e59b7021fb0fe8f
JEETU KANHAIYA PRASAD <142589448+jeetubro-158@users.noreply.github.com>
2024-03-24 13:49:22 +0530

    Add files via upload

---
+++ Practical -2/P2Q2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+class Time{
+    int hours, min, sec;
+    public:
+    Time(): hours(0), min(0), sec(0) {}
+    Time (int h, int m, int s){
+        hours = h;
+        min = m;
+        sec = s;
+    }
+    void disp(){
+        cout<< hours <<":"<< min <<":"<< sec <<endl;
+    }
+    Time addtime(Time t){
+        Time temp;
+        int h=0,m=0;
+        temp.sec = sec + t.sec;
+        if(temp.sec > 60){
+            temp.sec = temp.sec - 60;
+            m = 1;
+        }
+        temp.min = min + t.min+ m;
+        if(temp.min > 60){
+            temp.min = temp.min - 60;
+            h = 1;
+        }
+        temp.hours = hours + t.hours + h;
+        return temp;        
+    }
+};
+int main(){
+    Time t1(5,50,40);
+    t1.disp();
+    Time t2(4, 40, 50);
+    t2.disp();
+    Time t3 = t1.addtime(t2);
+    t3.disp();
+    return 0;
+}
