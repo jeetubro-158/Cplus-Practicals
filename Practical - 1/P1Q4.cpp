c500b8bff2b14e1b9ba88da7ecdfb86afccfef9a
JEETU KANHAIYA PRASAD <142589448+jeetubro-158@users.noreply.github.com>
2024-04-15 20:24:10 +0530

    Update P1Q4.cpp

--- Practical - 1/P1Q4.cpp
+++ Practical - 1/P1Q4.cpp
@@ -1,3 +1,8 @@
+/*Write an OOP in C++ to implement a class employee. An employee has a name and a salary (double).
+ Write a default constructor and constructor with two parameters (name and salary) and two methods:
+(i) to return the name and salary and
+(ii) A method that raises the employee's salary.*/
+
 #include <iostream>
 using namespace std;
 class employee{
@@ -9,32 +14,25 @@ class employee{
         name = n;
         salary = sal;
     }
-    string RtnName() {
-        return name;
-    }
-    double RtnSal(){
-        return salary;
+    void disp(){
+        cout << "Employee Name : " << name << endl;
+        cout << "Employee Salary : " << salary << endl;
     }
     void RaiseSal(double amount){
-        salary = salary + amount;
-    }
-    employee RtnBoth(){
-        return *this;
+        salary += amount;
     }
 };
+
 int main(){
     employee emp1("Agi", 1000);
     employee emp2("Kulkarni", 1500);
     cout << "Boefore Riasing the Salary : " << endl;
-    cout << "Emp 1 : " << emp1.RtnName() << " ---> " << emp1.RtnSal() << endl;
-    cout << "Emp 2 : " << emp2.RtnName() << " ---> " << emp2.RtnSal() << endl << endl;
-
+    emp1.disp();
+    emp2.disp();
+    cout << "After Riasing the Salary : " << endl;
     emp1.RaiseSal(500);
     emp2.RaiseSal(500);
-
-    cout << "After Riasing the Salary : " << endl;
-    cout << "Emp 1 : " << emp1.RtnName() << " ---> " << emp1.RtnSal() << endl ;
-    cout << "Emp 2 : " << emp2.RtnName() << " ---> " << emp2.RtnSal() << endl;
-    
+    emp1.disp();
+    emp2.disp();
     return 0;
 }
