68912b414dec24fd839ec28cffd275c31c8abdd7
JEETU KANHAIYA PRASAD <142589448+jeetubro-158@users.noreply.github.com>
2024-04-15 20:10:58 +0530

    Update P1Q5.cpp

--- Practical - 1/P1Q5.cpp
+++ Practical - 1/P1Q5.cpp
@@ -1,63 +1,47 @@
+/*Write an OOP in C++ to demonstrate the concept of using array of objects
+ to create an array of ten employees (reading their entire personal data: 
+ Name, age, emp_ID, gender, address) and print the same.*/
+
 #include <iostream>
-#include <string>
-using namespace std;
-class Employee {
-private:
-    string name;
-    int age;
-    string emp_ID;
+using  namespace std;
+class employee{
+    private:
+    string name,  address;
+    int age, Emp_id;
     char gender;
-    string address;
-
-public:
-    // Default constructor
-    Employee() : name(""), age(0), emp_ID(""), gender(' '), address("") {}
-
-    // Constructor with parameters
-    Employee(const string& name, int age, const string& emp_ID, char gender, const string& address)
-        : name(name), age(age), emp_ID(emp_ID), gender(gender), address(address) {}
-
-    // Method to display employee details
-    void display() const {
-        cout << "Name: " << name << endl;
-        cout << "Age: " << age << endl;
-        cout << "Employee ID: " << emp_ID << endl;
-        cout << "Gender: " << gender << endl;
-        cout << "Address: " << address << endl << endl;
+    public:
+    void getData(){
+        cout << "Enter Name : ";
+        cin >> name;
+        cout << "Enter Age : ";
+        cin >> age;
+        cout << "Enter emp ID : ";
+        cin >> Emp_id;
+        cout << "Enter Gender(M/F) : ";
+        cin >> gender;
+        cout << "Enter Address : ";
+        cin >> address;
+        cout << "\n";
+    }
+    void disp(){
+        cout << "Name : " << name << endl;
+        cout << "Age : " << age << endl;
+        cout << "Emp ID : " << Emp_id << endl;
+        cout << "Gender : " << gender << endl;
+        cout << "Address : " << address << endl;
+        cout << "\n";
     }
 };
 
-int main() {
-    // Array of 2 Employee objects
-    Employee employees[5];
-
-    // Reading data for each employee
-    for (int i = 0; i < 5; i++) {
-        string name, emp_ID, address;
-        int age;
-        char gender;
-
-        cout << "Enter details for Employee " << i + 1 << endl;
-        cout << "Name: ";
-        getline(cin >> ws, name); // Using getline to read the entire line including spaces
-        cout << "Age: ";
-        cin >> age;
-        cout << "Employee ID: ";
-        cin >> emp_ID;
-        cout << "Gender (M/F): ";
-        cin >> gender;
-        cout << "Address: ";
-        getline(cin >> ws, address); // Using getline to read the entire line including spaces
-
-        employees[i] = Employee(name, age, emp_ID, gender, address);
+int main(){
+    employee e1[5];
+    for (int i=0; i<5; i++){
+        cout << "Enter details for Employee-" << i+1 << ":"<< endl;
+        e1[i].getData();
     }
-
-    // Printing data for each employee
-    cout << "\nEmployee Details:\n";
-    for (int i = 0; i < 5; i++) {
-        cout << "Employee " << i + 1 << ":\n";
-        employees[i].display();
+    for (int i=0; i<5; i++){
+        cout << "Deails for Employee-" << i+1 << " :"<< endl;
+        e1[i].disp();
     }
-
     return 0;
 }
